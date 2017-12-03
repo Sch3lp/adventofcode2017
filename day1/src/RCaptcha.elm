d19ad1a55fdd4c2d4acab65de41013dec8111eab
= <sch3lp@gmail.com>
2017-12-03 15:01:54 +0100

    cleanup

---
+++ day1/src/RCaptcha.elm
@@ -0,0 +1,54 @@
+module RCaptcha exposing (..)
+
+import Array exposing (..)
+import Regex exposing (..)
+
+
+rcaptcha : String -> Int
+rcaptcha input =
+    let
+        inputAsArray =
+            input
+                |> Regex.split All (regex "")
+                |> List.filterMap (String.toInt >> Result.toMaybe)
+                |> Array.fromList
+    in
+        rcaptchaHelper inputAsArray
+
+
+rcaptchaHelper : Array Int -> Int
+rcaptchaHelper sequence =
+    sequence
+        |> Array.indexedMap (retainIfMatchesNext sequence)
+        -- |> Debug.log "indexedMap:"
+        |> Array.foldl (+) 0
+
+
+retainIfMatchesNext : Array Int -> (Int -> Int -> Int)
+retainIfMatchesNext seq =
+    \idx a ->
+        let
+            elementAtNextIndex =
+                Array.get (idx + 1) seq
+        in
+            case elementAtNextIndex of
+                Just b ->
+                    if a == b then
+                        a
+                    else
+                        0
+
+                Nothing ->
+                    let
+                        first =
+                            Array.get 0 seq
+                    in
+                        case first of
+                            Just b ->
+                                if a == b then
+                                    a
+                                else
+                                    0
+
+                            _ ->
+                                0
