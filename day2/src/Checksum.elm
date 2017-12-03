4bc131b3505aa3c30dd208dbc40f35928518a3e0
= <sch3lp@gmail.com>
2017-12-03 16:29:51 +0100

    day2 part 1 complete!

--- day2/src/Checksum.elm
+++ day2/src/Checksum.elm
@@ -1,5 +1,20 @@
 module Checksum exposing (..)
 
 
-snarf =
-    "snarf snarf"
+checksum : List (List Int) -> Int
+checksum spreadsheet =
+    spreadsheet
+        |> List.map rowDiff
+        |> List.foldl (+) 0
+
+
+rowDiff : List Int -> Int
+rowDiff row =
+    let
+        max =
+            List.maximum row |> Maybe.withDefault 0
+
+        min =
+            List.minimum row |> Maybe.withDefault 0
+    in
+        max - min
