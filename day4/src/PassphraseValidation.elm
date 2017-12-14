22168614215f032a2dd405d97782d39b027ca95e
= <sch3lp@gmail.com>
2017-12-14 16:37:57 +0100

    day 4.1 solved

--- day4/src/PassphraseValidation.elm
+++ day4/src/PassphraseValidation.elm
@@ -1,6 +1,31 @@
 module PassphraseValidation exposing (..)
 
+import Set exposing (..)
 
-numberOfInvalidPassphrases : String -> Int
-numberOfInvalidPassphrases passphrases =
-    0
+
+type alias Passphrase =
+    List String
+
+
+numberOfValidPassphrases : String -> Int
+numberOfValidPassphrases input =
+    splitIntoPassphrases input
+        |> List.filter isValid
+        |> List.length
+
+
+splitIntoPassphrases : String -> List Passphrase
+splitIntoPassphrases input =
+    String.lines input
+        |> List.map String.trim
+        |> List.filter (not << String.isEmpty)
+        |> List.map String.words
+
+
+isValid : Passphrase -> Bool
+isValid passphrase =
+    let
+        uniqueWords =
+            Set.fromList passphrase
+    in
+        List.length passphrase == Set.size uniqueWords
