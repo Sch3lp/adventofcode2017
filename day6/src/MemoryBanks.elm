aa156c67bd0dd6b07b89b57660b3b696ae21b707
= <sch3lp@gmail.com>
2017-12-17 22:28:29 +0100

    tests for finding the Bank with most Blocks

--- day6/src/MemoryBanks.elm
+++ day6/src/MemoryBanks.elm
@@ -2,13 +2,44 @@ module MemoryBanks exposing (..)
 
 
 type alias Banks =
-    List Int
+    List Bank
+
+
+banksFrom : List Block -> Banks
+banksFrom =
+    List.indexedMap Bank
 
 
 type Redistributions
     = Redistributions Int
 
 
+type alias Block =
+    Int
+
+
+type alias Index =
+    Int
+
+
+type Bank
+    = Bank Index Block
+
+
 solve : Banks -> Redistributions
 solve banks =
     Redistributions 0
+
+
+bankWithMostBlocks : Banks -> Bank
+bankWithMostBlocks banks =
+    Bank 1 1
+
+
+
+-- In each cycle, it finds the memory bank with the most blocks (ties won by the lowest-numbered memory bank) and redistributes those blocks among the banks. To do this, it removes all of the blocks from the selected bank, then moves to the next (by index) memory bank and inserts one of the blocks. It continues doing this until it runs out of blocks; if it reaches the last memory bank, it wraps around to the first one.
+
+
+redistribute : Block -> Banks -> Banks
+redistribute toDistribute banks =
+    banks
--- day6/tests/All.elm
+++ day6/tests/All.elm
@@ -7,10 +7,56 @@ import MemoryBanks exposing (..)
 
 solveTest : Test
 solveTest =
+    describe "solve"
+        [ test "given puzzle input" <|
+            \_ ->
+                banksFrom [ 5, 1, 10, 0, 1, 7, 13, 14, 3, 12, 8, 10, 7, 12, 0, 6 ]
+                    |> solve
+                    |> Expect.equal (Redistributions 1)
+        ]
+
+
+bankWithMostBlocksTest : Test
+bankWithMostBlocksTest =
+    describe "bankWithMostBlocks"
+        [ test "[0,5,2,9,1] => Bank at index 3 with 9" <|
+            \_ ->
+                banksFrom [ 0, 5, 2, 9, 1 ]
+                    |> bankWithMostBlocks
+                    |> Expect.equal (Bank 3 9)
+        , test "[0,9,2,9,1] => Bank at index 1 with 9" <|
+            \_ ->
+                banksFrom [ 0, 5, 2, 9, 1 ]
+                    |> bankWithMostBlocks
+                    |> Expect.equal (Bank 1 9)
+        , test "[0,0] => Bank at index 0 with 0" <|
+            \_ ->
+                banksFrom [ 0, 0 ]
+                    |> bankWithMostBlocks
+                    |> Expect.equal (Bank 0 0)
+        ]
+
+
+redistributeTest : Test
+redistributeTest =
     skip <|
-        describe "solve"
-            [ test "given puzzle input" <|
+        describe "redistribute"
+            [ test "from the start [3,0,0] -> [1,1,1]" <|
                 \_ ->
-                    solve [ 5, 1, 10, 0, 1, 7, 13, 14, 3, 12, 8, 10, 7, 12, 0, 6 ]
-                        |> Expect.equal (Redistributions 1)
+                    banksFrom [ 3, 0, 0 ]
+                        |> redistribute 3
+                        |> Expect.equal
+                            (banksFrom [ 1, 1, 1 ])
+            , test "from the end [0,0,3] -> [1,1,1]" <|
+                \_ ->
+                    banksFrom [ 0, 0, 3 ]
+                        |> redistribute 3
+                        |> Expect.equal
+                            (banksFrom [ 1, 1, 1 ])
+            , test "from the middle [0,3,0] -> [1,1,1]" <|
+                \_ ->
+                    banksFrom [ 0, 3, 0 ]
+                        |> redistribute 3
+                        |> Expect.equal
+                            (banksFrom [ 1, 1, 1 ])
             ]
