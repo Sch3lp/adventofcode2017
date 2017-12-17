cad321f6f87cb391bff4ab2747a352b58e307039
= <sch3lp@gmail.com>
2017-12-17 13:05:10 +0100

    potential model

--- day5/src/Trampolines.elm
+++ day5/src/Trampolines.elm
@@ -1,5 +1,21 @@
 module Trampolines exposing (..)
 
 
-type alias Trampoline =
-    { jump : Bool }
+type alias Offset =
+    Int
+
+
+type Instruction
+    = Forwards Offset
+    | Backwards Offset
+    | Remain
+
+
+type alias Instructions =
+    List Instruction
+
+
+type alias Movement =
+    { instructions : Instructions
+    , stepsTaken : Int
+    }
